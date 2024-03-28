d362f20806cce56cbb4ed7008e506e9f599fd78d
Ayodele Kadiri <kadelcode@gmail.com>
2024-03-28 18:30:15 +0100

    Add page-specific test

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -35,7 +35,10 @@ app.get('/', function (req, res) {
 // About page
 app.get('/about', function (req, res) {
   var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-  res.render('about', { fortune: randomFortune });
+  res.render('about', {
+	  fortune: randomFortune,
+	  pageTestScript: '/qa/tests-about.js'
+  });
 });
 
 // 404 catch-all handler (middleware)
